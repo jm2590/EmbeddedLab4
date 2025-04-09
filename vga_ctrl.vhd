06a561a50fdfadf4aecbb2b66a10457588488100
jm2590 <jm2590@scarletmail.rutgers.edu>
2025-04-09 16:54:16 -0400

    Update vga_ctrl.vhd

--- vga_ctrl.vhd
+++ vga_ctrl.vhd
@@ -20,32 +20,43 @@ begin
         if rising_edge(clk) then
             if clk_en = '1' then
                 hcount_temp <= std_logic_vector(unsigned(hcount_temp) + 1);
-                 if unsigned(hcount_temp) = 800 then
-                hcount_temp <= (others => '0');
-            end if;
-            if unsigned(hcount_temp) = 0 then
-                vcount_temp <= std_logic_vector(unsigned(vcount_temp) + 1);
-                if unsigned(vcount_temp) = 525 then
+                if unsigned(hcount_temp) = 799 then
                     hcount_temp <= (others => '0');
+                    vcount_temp <= std_logic_vector(unsigned(vcount_temp) + 1);
+                    if unsigned(vcount_temp) = 524 then
+                        vcount_temp <= (others => '0');
+                        vid <= '1';
+
+                    else
+                        if (unsigned(vcount_temp) < 479) then
+                            vid <= '1';
+
+                        else
+                            vid <= '0';
+                        end if;
                     end if;
-            end if;
-            if (unsigned(hcount_temp) < 640) and (unsigned(vcount_temp) > 480) then
-                vid <= '1';
-            else 
-                vid <= '0';
-            end if;
-            if (unsigned(hcount_temp) < 752) and (unsigned(hcount_temp) > 654) then
-                hs <= '0';
-            else 
-                hs <= '1';
-            end if;
-            if (unsigned(vcount_temp) < 492) and (unsigned(hcount_temp) > 489) then
-                vs <= '0';
-            else 
-                vs <= '1';
+
+                    if (unsigned(vcount_temp) = 490) or (unsigned(vcount_temp) = 489) then
+                        vs <= '0';
+                    else
+                        vs <= '1';
+                    end if;
+
+                else
+                    if (unsigned(hcount_temp) < 639) and (unsigned(vcount_temp) < 480) then
+                        vid <= '1';
+                    else
+                        vid <= '0';
+                    end if;
+                end if;
+
+                if (unsigned(hcount_temp) < 751) and (unsigned(hcount_temp) > 654) then
+                    hs <= '0';
+                else
+                    hs <= '1';
+                end if;
             end if;
         end if;
-      end if;
     end process;
     hcount <= hcount_temp;
     vcount <= vcount_temp;
